f41d3e82161d4fb779b4950630ca2744c2031e7e
Dorsaf Chihi <chihi.dorsaf@example.com>
2025-10-01 17:09:56 +0100

    CI: update workflow and Karma to use ChromeHeadlessCI

--- Segus_Engineering_Frontend/karma.conf.js
+++ Segus_Engineering_Frontend/karma.conf.js
@@ -1,3 +1,6 @@
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/1.0/config/configuration-file.html
+
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -10,14 +13,19 @@ module.exports = function (config) {
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
-      jasmine: {},
-      clearContext: false
+      jasmine: {
+        // you can add configuration options for Jasmine here
+        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
+        // for example, you can disable the random execution with `random: false`
+        // or set a specific seed with `seed: 4321`
+      },
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     jasmineHtmlReporter: {
-      suppressAll: true
+      suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage'),
+      dir: require('path').join(__dirname, './coverage/segus-engineering'),
       subdir: '.',
       reporters: [
         { type: 'html' },
@@ -26,10 +34,6 @@ module.exports = function (config) {
       ]
     },
     reporters: ['progress', 'kjhtml'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
     browsers: ['Chrome'],
     customLaunchers: {
       ChromeHeadlessCI: {
@@ -39,11 +43,18 @@ module.exports = function (config) {
           '--disable-gpu',
           '--disable-dev-shm-usage',
           '--disable-software-rasterizer',
-          '--disable-extensions'
+          '--disable-extensions',
+          '--headless',
+          '--disable-translate',
+          '--disable-popup-blocking'
         ]
       }
     },
-    singleRun: false,
-    restartOnFileChange: true
+    restartOnFileChange: true,
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    singleRun: false
   });
 };
